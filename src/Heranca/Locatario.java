7ddcedbadefe9b259caf5329ae45a049c067832d
Henrique <joseguissoni@hotmail.com>
2025-05-07 19:58:44 -0300

    v1

---
+++ src/Heranca/Locatario.java
@@ -0,0 +1,12 @@
+// GABRIEL SANTOS ZAMPIERI
+// HENRIQUE GUISSONI
+
+package Heranca;
+
+class Locatario extends Pessoa{
+
+    public Locatario(String nome, String cpf, String endereco) {
+        super(nome, cpf, endereco);
+    }
+    
+}
